e2b40c4a2a391f01eeeeb8461bb566832aa3f06e
Saikiran Jamiligiri <saikiran.jamiligiri@felixsolutions.ai>
2024-06-04 11:18:38 +0530

    ui changes

--- demo-vl/src/components/HelloWorld.vue
+++ demo-vl/src/components/HelloWorld.vue
@@ -1,26 +1,31 @@
 <template>
-  <div class="bg-black">
-    <div class="flex banner">
+  <div class="flex flex-col bg-black min-h-96">
+    <div id="banner" class="flex w-full space-x-2 overflow-auto">
       <img v-for="n in 3" src="../assets/01.webp" />
     </div>
-    <div class="py-8 px-24">
-      <h1 class="text-white py-1.5 px-0">{{ secondRowTitle }}</h1>
-      <div class="flex banner">
-        <span v-for="asset in secondRowData" class="flex flex-col p-1 h-auto w-64 banner-card">
+    <div id="tiles_1" class="flex flex-col mb-2">
+      <div class="text-white text-lg font-bold sm:text-xl pl-2">{{ secondRowTitle }}</div>
+      <div class="flex flex-row my-2 overflow-auto">
+        <div v-for="asset in secondRowData" class="min-w-32 w-32 sm:w-64">
           <img :src="getImageUrl(asset)" />
-          <span class="text-white p-1 text-xs"> {{ asset.title }}</span>
-          <span class="type py-0.5 px-1"> {{ asset.params.genres[0].title }}</span>
-        </span>
+          <p class="text-white font-semibold px-2"> {{ asset.title }}</p>
+          <p class="px-2 text-gray-300"> {{ asset.params.genres[0].title }}</p>
+        </div>
       </div>
     </div>
-    <div class="py-8 px-24">
-      <h1 class="text-white py-1.5 px-0">{{ thirdRowTitle }}</h1>
-      <div class="flex banner">
-        <span v-for="asset in thirdRowData" class="flex flex-col p-1 h-auto w-64 banner-card">
-          <img :src="getImageUrl(asset)" />
-          <span class="text-white p-1 text-xs"> {{ asset.title }}</span>
-          <span class="type py-0.5 px-1"> {{ asset.params.genres[1]?.title }}</span>
-        </span>
+    <div id="tiles_with_rank" class="mb-5">
+      <div class="text-white text-lg sm:text-xl font-bold pl-2">{{ thirdRowTitle }}</div>
+      <div class="flex flex-row overflow-auto">
+        <div v-for="(asset, index) in thirdRowData" class="flex flex-row min-w-32 w-32 sm:w-64 ml-1">
+          <div class="text-[35px] font-bold text-white relative pr-2">
+            <span class="absolute bottom-12 sm:bottom-8 mr-1">{{ index+1 }}</span>
+          </div>
+          <div class="">
+          <img class="object-cover" :src="getImageUrl(asset)" />
+          <p class="px-2 font-semibold text-white"> {{ asset.title }}</p>
+          <p class="px-2 text-gray-300"> {{ asset.params.genres[1]?.title }}</p>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -54,22 +59,4 @@ function getImageUrl(asset: any) {
 <style scoped>
 @import url("https://m7cdn.io/common/css/external/external.css");
 
-.banner {
-  overflow: auto;
-  white-space: nowrap;
-}
-
-.banner::-webkit-scrollbar {
-  display: none;
-}
-
-.banner-card {
-  white-space: wrap;
-  min-width: 260px;
-}
-
-.type {
-  color: lightgrey;
-  font-size: 11px;
-}
 </style>
